f9dc8bfbf49f767525006dfd1d2a282adfb4c487
Mirnesa Salihovic <nessa.salihovic98@gmail.com>
2021-12-15 18:32:43 +0100

    commit

--- app.js
+++ app.js
@@ -19,11 +19,11 @@ var user;
   user= {username:socket.username,id:socket.id};
   next();   
 });
-
+*/
 
 io.on('connection', (socket) => {
 
-});*/
+});
 
 server.listen(port,() => {
   console.log(`Server running at port `+port);
